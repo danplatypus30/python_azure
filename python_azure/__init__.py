46c3255f901df3c4798792a2b6fcdd9aee2f2c51
danplatypus <daniel.gohch@hotmail.com>
2021-01-24 17:04:48 +0800

    chenges

--- python_azure/__init__.py
+++ python_azure/__init__.py
@@ -23,11 +23,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             else:
                 return func.HttpResponse(f"Hello, {name}. This text may be fake, consider cross checking it with other sources.")
         else:
-            return func.HttpResponse(f"Hello, {name}. Try to add a test parameter in your request for a personalised response.")
+            return func.HttpResponse(f"Hello, {name}. Try to add a bodyText parameter in your request for a more personalised response.")
     else:
         return func.HttpResponse(
              #"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              #try to use request { test:'insert string here' } for a personalised response
-             "HTTP Request triggered correctly, try to add a test parameter in your request for a personalised response.",
+             "HTTP Request triggered correctly, try to add a name parameter in your request for a personalised response.",
              status_code=200
         )
